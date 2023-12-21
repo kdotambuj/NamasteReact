88733aa5e4ff89ae81697ab94e7c09d773c07e05
Ambuj Kulshrestha <ambujdj666@gmail.com>
2023-12-21 10:10:34 +0530

    ep1

---
+++ App.js
@@ -0,0 +1,9 @@
+
+
+const heading = React.createElement("h1",{},[React.createElement("h2",{},"child1"),React.createElement("h2",{},"child2")]);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
