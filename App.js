4636d9443bf27cc950415f49040e5cd4f9b927db
Ambuj Kulshrestha <ambujdj666@gmail.com>
2023-12-23 20:42:31 +0530

    ep3

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
 
 import React from "react";
-import  ReactDOM  from "react-dom";
+import  ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1",{},[React.createElement("h2",{},"child1"),React.createElement("h2",{},"child2")]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// Core React
+const heading = React.createElement(
+    "h1",
+    {id:'heading'},
+    "Core React Heading"
+);
+
+
+// JSX heading
+// React Element
+const jsxHeading = (<h1 id="headig" tabIndex="9">JSX heading </h1>);
 
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// React functional Component
 
-root.render(heading);
+const Title = () =>{
+    return (
+        <h1>This is Title </h1>
+    )
+}
+
+const HeadingComponent = () =>{
+    return (
+        <>
+           <Title />
+           <h1>Hello from Heading Component</h1>
+        </>
+      
+    )
+}
+
+root.render(<HeadingComponent />)
+
+
+
+
+
+
+// const reactEle = React.createElement("h1",{},"Hello from core react");
+
+// const reactEle2 = (<h1>Hello from jsx Element, Also an React element </h1>)
+
+// const Mycomponent2 = () => {
+//     return (
+//         <> 
+//           <Title />
+//           <h1> Hello from Component</h1>
+//         </>
+//     )
+// }
+
+// root.render(<Mycomponent2 />)
+
